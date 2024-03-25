831f61c782c4c11d16d069cbcedbe3e8525c2c7f
Julio Wilfredo Rodríguez <jrwilly1986@gmail.com>
2024-03-25 00:10:53 -0600

    Modificacion del Index, Css y JavaScript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log("Hello from app.js!");
+//variable que mantinene el estado del menu
+let menuVisible = false;
+
+//funcion que oculta o mestra el menu
+function mostrarOcultar(){
+    if(menuVisible) {
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    } else {
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+//funcion para ocultar el menu cuando se selecciona una opcion
+function seleccionar(){
+    document.getElementById("nav").classList ="";
+    menuVisible = false;
+}
